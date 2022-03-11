44c71bd5cab61ade25ce5b24da2d415560121258
Jhonny-32 <jhoapoloche@misena.edu.co>
2022-03-10 19:56:03 -0500

    Docs: Se agrega nuevo Dashboard

--- pagina/models/conexion.php
+++ pagina/models/conexion.php
@@ -4,7 +4,7 @@
 
         public function __construct(){
             try{
-                parent::__construct('mysql:host=127.0.0.1;dbname=dbjohanstyle', 'root', '');
+                parent::__construct('mysql:host=localhost;dbname=johanstyle', 'root', 'angeles-32');
                 parent::exec("set names utf8");
             }catch(PDOException $e){
                 echo "Error al conectar " . $e->getMessage();
